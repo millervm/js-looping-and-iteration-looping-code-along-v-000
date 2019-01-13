43510a67cc58aac281fa60d745280c2b1a2295a8
Veronica Miller <vmcmiller@gmail.com>
2019-01-13 21:44:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function printBadges(employees) {
+  for (let i = 0; i < employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${[i + 1]}.`);
+  }
+  return employees;
+}
+
+function tails() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let tailsCount = 0;
+  while (tails()) {
+    tailsCount++;
+  }
+  return `You got ${tailsCount} tails in a row!`;
+}
